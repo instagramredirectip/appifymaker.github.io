ba712c8ebcd5b41b8a626585c83f97c926ba8737
instagramredirectip <karthikanilcolab@gmail.com>
2025-03-07 15:35:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function selectTemplate(template) {
+    // Redirect to the template selection page or show a modal with the form
+    alert(`You selected the ${template} template!`);
+    // Here you can implement the logic to show the form dynamically
+}
