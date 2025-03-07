2f6d5c7a939956a37b620b20c20b305df0f00546
instagramredirectip <karthikanilcolab@gmail.com>
2025-03-07 16:26:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// script.js
+﻿// script.js
 function selectTemplate(template) {
     // Redirect to the template selection page or show a modal with the form
-    alert(`You selected the ${template} template!`);
+   // alert(`You selected the ${template} template!`);
+
+    if (template == "url-android") {
+        window.location.href = "https://forms.gle/FXa6B8eJc3yDxnLB6";
+
+    }
+    else {
+        alert(` The ${template} is not available 🥲`);
+
+    }
     // Here you can implement the logic to show the form dynamically
 }
